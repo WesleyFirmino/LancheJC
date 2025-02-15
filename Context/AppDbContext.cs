d0cfaf22fb5d0a9b53be2d62ff0aecfd675c7a9e
WesleyFirmino <37452899+WesleyFirmino@users.noreply.github.com>
2025-02-15 14:42:02 -0300

    Initial commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesJC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesJC.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Lanche> Lanches { get; set; }
+}
---
+++ Views/Shared/_Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LanchesJC.Views.Shared
+{
+    public class _CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
