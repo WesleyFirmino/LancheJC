8f0875e95b000c3235541755b9e4430fa12b066a
WesleyFirmino <37452899+WesleyFirmino@users.noreply.github.com>
2025-02-27 09:42:37 -0300

    Criacao View/Controller Lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesJC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJC.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILanchesRepository _lanchesRepository;
+
+        public LancheController(ILanchesRepository lanchesRepository)
+        {
+            _lanchesRepository = lanchesRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lanchesRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
