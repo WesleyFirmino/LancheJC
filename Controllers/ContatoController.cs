6d2af24f05c81782437f342dfbdff08fdec3a910
WesleyFirmino <37452899+WesleyFirmino@users.noreply.github.com>
2025-03-04 11:22:58 -0300

    Criação da area do Contato, criação de um TagHelper para envio de e-mail

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJC.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
