8bf46c0d2f7cabaef7674d3de7bae754b4c8047f
WesleyFirmino <37452899+WesleyFirmino@users.noreply.github.com>
2025-03-05 15:39:16 -0300

    Criacao PedidoController e implementacao do Checkout

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesJC.Models;
+using LanchesJC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJC.Controllers;
+public class PedidoController : Controller
+{
+    private readonly IPedidoRepository _pedidoRepository;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+    {
+        _pedidoRepository = pedidoRepository;
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Pedido pedido)
+    {
+        return View();
+    }
+}
